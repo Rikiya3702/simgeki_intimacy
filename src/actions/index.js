48d2682776778f3384f39429c46563e0912ca882
Rikiya3702 <rikiya3702@gmail.com>
2020-03-13 16:09:21 +0900

    CountComponent with Redux

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export const INCREMENT = "INCREMENT"
+export const DECREMENT = "DENCREMENT"
+
+export const increment = () => ({
+  type: INCREMENT
+})
+export const decrement = () => ({
+  type: DECREMENT
+})
---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+
+import { increment, decrement } from '../actions'
+
+class App extends Component {
+  render(){
+    const props = this.props
+
+    return (
+      <div className="App">
+        <div>Hello: { props.value }</div>
+        <button onClick={ props.increment }>+1</button>
+        <button onClick={ props.decrement }>-1</button>
+
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({ value: state.count.value })
+
+// const mapDispatchToProps = dispatch => ({
+//   increment: () => dispatch(increment()),
+//   decrement: () => dispatch(decrement()),
+// })
+const mapDispatchToProps = ({
+  increment,
+  decrement
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
